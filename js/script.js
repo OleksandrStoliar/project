2cc1fba5e16ef6134c0a1bdb5e75c983758f65f7
Sasha <example@gmail.com>
2021-07-27 17:30:48 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+"use strict";
+
+//alert('Hello');
+
+//const result = confirm('Are you here?');
+//console.log(result);
+
+//const answer = +prompt('Are you older than 18?','18');
+//console.log(typeof(answer));
+
+const answers = [];
+
+//answers[0] = prompt("What's your name?","");
+//answers[1] = prompt("What's your last name?","");
+//answers[2] = prompt("What's your age?","");
+
+//const category = 'toys';
+
+//console.log(`https://someurl.com/${category}/5`);
+
+//const user = 'Sasha';
+
+//alert(`Hello, ${user}`);
+
+//let incr = 10,
+//   decr = 10;
+
+
+//console.log(incr++);
+//console.log(decr--);
+
+const isChecked = false,
+      isClose = true;
+
+//console.log(isChecked && isClose && isClose);
+console.log(isClose || isChecked || isClose);
+      
