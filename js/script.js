3bafa5ccbd7ae31ace8a4043680bce0866954bac
Sasha <example@gmail.com>
2021-07-29 16:19:07 +0300

    First practice task was completed. File script.js was modified.

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@
 //const answer = +prompt('Are you older than 18?','18');
 //console.log(typeof(answer));
 
-const answers = [];
+//const answers = [];
 
 //answers[0] = prompt("What's your name?","");
 //answers[1] = prompt("What's your last name?","");
@@ -29,9 +29,33 @@ const answers = [];
 //console.log(incr++);
 //console.log(decr--);
 
-const isChecked = false,
-      isClose = true;
+//const isChecked = false,
+//      isClose = true;
 
 //console.log(isChecked && isClose && isClose);
-console.log(isClose || isChecked || isClose);
-      
+//console.log(isClose || isChecked || isClose);
+
+
+
+
+//Практика №1
+
+//first task 1
+const numerOfFilms = +prompt("How many films did you watch?", "");
+
+//second task 2
+const personalMovieDB = {
+    count: numerOfFilms,
+    movies: {},
+    actors: {},
+    ganres: [],
+    privat: false
+};
+
+//third task 3
+const lastWatchedFilm = prompt("Which film did you watch last?", ""),
+      rateLastFilm = prompt("Put the rait of last watched film", "");
+
+personalMovieDB.movies[lastWatchedFilm] = rateLastFilm;
+
+console.log(personalMovieDB);
